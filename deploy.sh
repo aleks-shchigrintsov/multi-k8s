33a6458574aafe65dcbb2cd0b7fd60068684a9f4
Aleksandr Shchigrintsov <shchigrintsov@gmail.com>
2019-02-17 13:08:54 +0100

    change deploy file

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,8 @@
+
 docker build -t shchigrintsov/multi-client:latest -t shchigrintsov/multi-client:$SHA -f ./client/Dockerfile ./client
 docker build -t shchigrintsov/multi-server:latest -t shchigrintsov/multi-server:$SHA -f ./server/Dockerfile ./server
 docker build -t shchigrintsov/multi-worker:latest -t shchigrintsov/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
 docker push shchigrintsov/multi-client:latest
 docker push shchigrintsov/multi-server:latest
 docker push shchigrintsov/multi-worker:latest
@@ -10,6 +12,6 @@ docker push shchigrintsov/multi-server:$SHA
 docker push shchigrintsov/multi-worker:$SHA
 
 kubectl apply -f k8s
-kubectl set image shchigrintsov/server-deployment server=shchigrintsov/multi-server:$SHA
-kubectl set image shchigrintsov/client-deployment client=shchigrintsov/multi-client:$SHA
-kubectl set image shchigrintsov/worker-deployment worker=shchigrintsov/multi-worker:$SHA
+kubectl set image deployments/server-deployment server=shchigrintsov/multi-server:$SHA
+kubectl set image deployments/client-deployment client=shchigrintsov/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=shchigrintsov/multi-worker:$SHA
